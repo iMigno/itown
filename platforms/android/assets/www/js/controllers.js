e186e2c69bd3dc08cd00cbaae0ec0e08cee91bbb
iMigno <mignogna.alessandro@gmail.com>
2016-04-22 13:02:06 +0200

    add simple rss reader + android platform

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,85 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller("RssController", function($http, $scope) {
+ 
+    $scope.init = function() {
+         $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.comune.biccari.fg.it/moduli/rss_notizie.php" } })
+        //$http.jsonp("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "num":"10", "q": "http://www.cantiribelli.com/feed" } })
+            .success(function(data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                window.localStorage["entries"] = JSON.stringify(data.responseData.feed.entries);
+            })
+            .error(function(data) {
+                console.log("ERROR: " + data);
+                if(window.localStorage["entries"] !== undefined) {
+                    $scope.entries = JSON.parse(window.localStorage["entries"]);
+                }
+            });
+    }
+
+    $scope.browse = function(v) {
+      window.open(v, "_system", "location=yes");
+    }
+ 
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
+
+
+
